78dfb00a93baf120788da5a9a3d47338788e166a
dragsec <draganyes@gmail.com>
2021-06-22 02:58:16 +0200

    cose

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once "./classes/products/smartphoneProduct.php";
+require_once "./classes/user.php";
+require_once "./classes/cart.php"
+require_once "./classes/users/primeUser.php";
+
+$user = new PrimeUser("Mario Rossi", "mario.rossi@gmail.com");
+$cart = new Cart($user);
+$creditCard = new CreditCard(1234123412341234, '12/2012', 666);
+
+
+$SGS21 =new SmartphoneProduct("Galaxy", 1, 950, "S21", "Samsung");
+$cart->addProduct($SGS21);
+
+$redmi9 = new SmartphoneProduct("redmi", 1, 200, "9", "Xiaomi");
+$cart->addProduct($redmi9);
+
+var_dump($cart);
